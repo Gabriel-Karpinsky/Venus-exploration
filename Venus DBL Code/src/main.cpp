11e4ee775529d411a0bf9070cb82840b93b60cbc
mega <35005210+megabro@users.noreply.github.com>
2023-05-17 14:00:55 +0200

    Add basics, again

--- Venus DBL Code/src/main.cpp
+++ Venus DBL Code/src/main.cpp
@@ -1,153 +1,45 @@
-#include <Servo.h>
 #include <Arduino.h>
+#include <Servo.h>
 
-Servo gripperServo;
-int gripper(int a,int b){
-  //Servo called: gripperServo
-  //a=robot,b=action (0==close,1==open,2==up)
-  if(a==0){
-    if(b==0) gripperServo.write(70);
-    else if (b==1) gripperServo.write(170);
-    else if (b==2) gripperServo.write(0);
-    delay(2000);
-  }
-  else if(a==1){
-    if(b==0) gripperServo.write(0);
-    else if (b==1) gripperServo.write(80);
-    else if (b==2) gripperServo.write(180);
-    delay(2000);
-  }
-}
-Servo leftServo;
-Servo rightServo;
-int movement(int a,int b){
-  //Servo called: leftServo and rightServo
-  //a==robot,b==action (0==stop,1==forward,2==backward,3==left,4==right)
-  if(a==0){
-    if(b==0){
-      leftServo.writeMicroseconds(1500);
-      rightServo.writeMicroseconds(1500);
-    }
-    else if(b==1){
-      leftServo.writeMicroseconds(1700);
-      rightServo.writeMicroseconds(1300);
-    }
-    else if(b==2){
-      leftServo.writeMicroseconds(1300);
-      rightServo.writeMicroseconds(1700);
-    }
-    else if(b==3){
-      leftServo.writeMicroseconds(1300);
-      rightServo.writeMicroseconds(1300);
-    }
-    else if(b==4){
-      leftServo.writeMicroseconds(1700);
-      rightServo.writeMicroseconds(1700);
-    }
-  }
-  else if(a==1){
-    if(b==0){
-      leftServo.writeMicroseconds(1460);
-      rightServo.writeMicroseconds(1520);
-    }
-    else if(b==1){
-       leftServo.writeMicroseconds(1700);
-      rightServo.writeMicroseconds(1300);
-    }
-    else if(b==2){
-      leftServo.writeMicroseconds(1300);
-      rightServo.writeMicroseconds(1700);
-    }
-    else if(b==3){
-      leftServo.writeMicroseconds(1300);
-      rightServo.writeMicroseconds(1300);
-    }
-    else if(b==4){
-      leftServo.writeMicroseconds(1700);
-      rightServo.writeMicroseconds(1700);
-    }
-  }
-}
-Servo ultrasonicServo;
-int posServoUltra=0;
-int posServoUltraDir=0;
+#ifdef WHACKY_ROBOT
+#  define LEFT_STATIONARY  1460
+#  define RIGHT_STATIONARY 1520
+#else
+#  define LEFT_STATIONARY  1500
+#  define RIGHT_STATIONARY 1500
+#endif
 
-int ultrasonic(int a,int b,int c,int d){
-  //Servo called: ultrasonicServo
-  //a==robot,b==boundA,c==boundB,d=increment
-  if(a==0){
-    if(posServoUltra<b || posServoUltra>c){
-      if(posServoUltraDir==0)posServoUltraDir=1;
-      else if(posServoUltraDir==1)posServoUltraDir=0;
-    }
-    if(posServoUltraDir==0){
-      for(int i=posServoUltra;i<=c;i++){
-        ultrasonicServo.write(i);
-        posServoUltra=i+d;
-        break;
-      }
-    }
-    else if(posServoUltraDir==1){
-      for(int i=posServoUltra;i>=b;i--){
-        ultrasonicServo.write(i);
-        posServoUltra=i-d;
-        break;
-      }
-    }
-  }
-  else if(a==1){
-    if(posServoUltra<b || posServoUltra>c){
-      if(posServoUltraDir==0)posServoUltraDir=1;
-      else if(posServoUltraDir==1)posServoUltraDir=0;
-    }
-    if(posServoUltraDir==0){
-      for(int i=posServoUltra;i<=c;i++){
-        ultrasonicServo.write(i);
-        posServoUltra=i+d;
-        break;
-      }
-    }
-    else if(posServoUltraDir==1){
-      for(int i=posServoUltra;i>=b;i--){
-         ultrasonicServo.write(i);
-        posServoUltra=i-d;
-        break;
-      }
-    }
-  }
-//Moving UltrasonicServo can be probably cleaner/better (but it works)
-  
-  pinMode(9,OUTPUT);
-  digitalWrite(9,LOW);
-  delayMicroseconds(2);
-  digitalWrite(9, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(9, LOW);
-  pinMode(9, INPUT);
-  int duration=pulseIn(9, HIGH);
-  int dis_cm=duration/29/2;
-  Serial.println("Distance="+String(dis_cm)+" cm   Angle="+String(posServoUltra));
-  return dis_cm;
+// Pins
+const int left_servo_pin         = 13;
+const int right_servo_pin        = 12;
+const int ultrasound_servo_pin   = 11;
+const int claw_servo_pin         = 10;
+const int ultrasound_pin         = 9;
+const int left_wheel_sensor_pin  = 8;
+const int right_wheel_sensor_pin = 7;
+
+// Components
+Servo left_servo;
+Servo right_servo;
+
+// Inputs
+uint32_t front_ultrasound_distance;
+
+int get_ultrasound_distance();
+
+void setup()
+{
+    Serial.begin(115200);
+
+    left_servo.attach(left_servo_pin);
+    right_servo.attach(right_servo_pin);
+
+    left_servo.writeMicroseconds(LEFT_STATIONARY);
+    right_servo.writeMicroseconds(RIGHT_STATIONARY);
+
 }
 
-
-void setup() {
-  Serial.begin(9600);
-  gripperServo.attach(10);
-  leftServo.attach(12);
-  rightServo.attach(13);
-  ultrasonicServo.attach(11);
-}
-
-void loop() {
-  movement(0,1);
-    if(ultrasonic(0,50,120,5)<=20){
-      if(posServoUltra>90){
-        movement(0,4);
-        delay(200);
-      } else if(posServoUltra<90){ 
-        movement(0,3);
-        delay(200);
-        }
-    }
+void loop()
+{
+    get_ultrasound_distance();
 }
