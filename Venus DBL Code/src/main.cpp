b733e52cbc3b2fad552c95f59e98a2da48d9a738
mega <35005210+megabro@users.noreply.github.com>
2023-05-17 14:24:42 +0200

    oops

--- Venus DBL Code/src/main.cpp
+++ Venus DBL Code/src/main.cpp
@@ -39,13 +39,6 @@ void loop()
 {
 }
 
-long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the object we
-  // take half of the distance travelled.
-
-}
-
 float get_ultrasound_distance_cm()
 {
     long duration;
